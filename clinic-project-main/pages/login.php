7cfcd7302a9e0a93c62019db7b7f0b65c426c5aa
KrisNathanielDimaapi <113610705+KrisNathanielDimaapi@users.noreply.github.com>
2024-11-21 17:03:35 +0800

    Delete clinic-project-main directory

--- clinic-project-main/pages/login.php
+++
@@ -1,9 +0,0 @@
-<?php
-    session_start();
-
-    if (!isset($_SESSION['loggedIn']) || $_SESSION['loggedIn'] !== true) {
-        header('Location: loginReg.php');
-        exit();
-
-    }
-?>
--- clinic-project-main/scripts/loginLoad.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("load", function(){
-    let rightS = document.getElementById("right");
-    const newImg = document.createElement("img");
-    newImg.src = "../images/heroClinic.png";
-    newImg.id = "rightPic";
-    newImg.className = "heroClinicImg";
-    document.getElementById("right").insertBefore(newImg, rightS.firstChild);
-});
